0f5e45f49d3143aab2855c12281a3be5de2bbdf9
phferreira <p.h.ferreirah@gmail.com>
2018-09-08 20:21:35 -0300

    Adicionado projeto

--- js/jquery.js
+++ js/jquery.js
@@ -1,51 +1,21 @@
-var player1 = 'O';
-var player2 = 'X';
-var player_atual = player1;
-
-var verificar = function(a, b, c){
-  if (($('td').eq(a).text() == player_atual) &&
-     ($('td').eq(b).text() == player_atual) &&
-     ($('td').eq(c).text() == player_atual))
-  {
-    $('td').eq(a).addClass('destaque');
-    $('td').eq(b).addClass('destaque');
-    $('td').eq(c).addClass('destaque');
-    return true;
-  }
-}
-
-$.fn.MyFunction = function(){
-  return (
-    verificar(0,1,2) ||
-    verificar(3,4,5) ||
-    verificar(6,7,8) ||
-    verificar(0,3,6) ||
-    verificar(1,4,7) ||
-    verificar(2,5,8) ||
-    verificar(0,4,8) ||
-    verificar(2,4,6));
-}
-
 $(document).ready(function(){
 
-  $('td').text('');
+  //~ Menu
+  $('.sub-item').hide();
 
-  $('td')
-
-
-  $('td').click(function(){
-    if ($(this).text() == '') {
-      $(this).text(player_atual);
-
-      if ($.fn.MyFunction()){
-        $('td').unbind('click');
-      }
-
-      if (player_atual == player1){
-        player_atual = player2
-      }else{
-        player_atual = player1;
-      } return true;
-    }
+  $('.menu-item').click(function(){
+    $(this).next('ul').slideToggle();
   });
+
+  $('.flexslider').flexslider({
+    controlNav: false,
+    directionNav: false,
+    slideshowSpeed: 3000,
+    itemWidth: 5000,
+  });
+
+  $('#topo').click(function() {
+    $('html, body').animate({scrollTop:0}, 'slow');
+  });
+
 });
