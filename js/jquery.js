a31cef2df4782c4254cffdc02926b04bb56b4e58
phferreira <p.h.ferreirah@gmail.com>
2018-09-10 20:54:29 -0300

    Ajustes css-jquery

--- js/jquery.js
+++ js/jquery.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('.sub-item').hide();
 
   $('.menu-item').click(function(){
-    $(this).next('ul').slideToggle();
+    $(this).next('.sub-item').slideToggle();
   });
 
   $('.flexslider').flexslider({
@@ -18,4 +18,30 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop:0}, 'slow');
   });
 
+  $('#datanasc').datepicker();
+
+  $.getJSON('estados_cidades.json', function (data) {
+    var items = [];
+    var options = '<option value="">escolha um estado</option>';
+    $.each(data, function (key, val) {
+      options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+    });
+
+    $("#estados").html(options);
+    $("#estados").change(function () {
+      var options_cidades = '';
+      var str = "";
+      $("#estados option:selected").each(function () {
+        str += $(this).text();
+      });
+      $.each(data, function (key, val) {
+        if(val.nome == str) {
+          $.each(val.cidades, function (key_city, val_city) {
+            options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
+          });
+        }
+      });
+      $("#cidades").html(options_cidades);
+    }).change();
+  });
 });
